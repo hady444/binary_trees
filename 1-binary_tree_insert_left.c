445f13801a328d63f129eea6a5e40ceb3e9f9bd9
hady444 <hadyhassan2312@gmail.com.com>
2024-02-01 19:04:23 +0000

    1.Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,18 +5,14 @@
  * @value: int value stored in node.
  *
  * Return: Created node or NULL on failure.
- */
+*/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL)
+	if (parent == NULL)
 		return (NULL);
-	node->parent = parent;
-	node->n = value;
-	node->left = NULL;
-	node->right = NULL;
+	node = binary_tree_node(parent, value);
 	if (parent->left != NULL)
 	{
 		node->left = parent->left;
