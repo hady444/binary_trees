e8c1fc3263e1494052c1c9d93a093d65ca367504
hady444 <hadyhassan2312@gmail.com.com>
2024-02-01 19:08:22 +0000

    2.Insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,16 +8,16 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        binary_tree_t *node;
+	binary_tree_t *node;
 
-        if (parent == NULL)
-                return (NULL);
-        node = binary_tree_node(parent, value);
-        if (parent->right != NULL)
-        {
-                node->right = parent->right;
-                node->right->parent = node;
-        }
-        parent->right = node;
-        return (node);
+	if (parent == NULL)
+		return (NULL);
+	node = binary_tree_node(parent, value);
+	if (parent->right != NULL)
+	{
+		node->right = parent->right;
+		node->right->parent = node;
+	}
+	parent->right = node;
+	return (node);
 }
