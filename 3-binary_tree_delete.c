0f9005bc85190d87b301e2086a03e56bd7091cd9
hady444 <hadyhassan2312@gmail.com.com>
2024-02-01 19:50:04 +0000

    3. Delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,7 +9,6 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree->left)
 		binary_tree_delete(tree->left);
 	if (tree->right)
-                binary_tree_delete(tree->right);
+		binary_tree_delete(tree->right);
 	free(tree);
-	return;
 }
