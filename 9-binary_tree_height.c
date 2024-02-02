a77833653a8a139c18cc420befdade05b2e86212
hady444 <hadyhassan2312@gmail.com.com>
2024-02-02 13:24:00 +0000

    9. Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left || tree->right)
-		return (1 + max(binary_tree_height(tree->left), binary_tree_height(tree->right)));
+		return (1 + max(binary_tree_height(tree->left),
+					binary_tree_height(tree->right)));
 	else
 		return (0);
 }
